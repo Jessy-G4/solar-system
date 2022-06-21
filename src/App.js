43429ba8163abb9ab3b516724f9a3c56de64535d
Jessy Cristina <jessyaraujo395@gmail.com>
2022-06-21 19:04:38 -0300

    requisito 8 concluido

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
-import PlanetCard from './components/PlanetCard';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
@@ -11,8 +11,8 @@ class App extends React.Component {
         <p>Sistema Solar</p>
         <Header />
         <SolarSystem />
+        <Missions />
         <Title headline="" />
-        <PlanetCard />
       </>
     );
   }
